31831a84d63dd736a67cefcea30fb363cdd9c4cb
qinyajie <qinyajie@msn.com>
2018-02-27 14:51:24 +0800

    prepare fore task 3_2

---
+++ challenge3_2.py
@@ -0,0 +1,17 @@
+# /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import datetime
+
+def combine():
+    pass
+
+def split():
+    pass
+
+
+if __name__ == '__main__':
+    combine()
+    split()
