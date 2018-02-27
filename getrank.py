a21d491ec8828424d04ebb071aa1c0f4f37f4ee8
qinyajie <qinyajie@msn.com>
2018-02-27 14:38:38 +0800

    pass:getrank use mongodb

--- getrank.py
+++ getrank.py
@@ -20,7 +20,7 @@ def get_rank(usr_id):
     result = list(db.contests.aggregate(pipe_line1))
     if(len(result) == 0):
         return 0, 0, 0
-    date = result[0]
+    data = result[0]
 
     pipe_line2 = [
             {
@@ -28,21 +28,34 @@ def get_rank(usr_id):
                     '_id':'$user_id',
                     'total_score':{'$sum':'$score'},
                     'total_time':{'$sum':'$submit_time'}
-                    },
+                    }
+            },
+            {
                 '$match':{
                     '$or':[
                         {'total_score':{'$gt':data['total_score']}},
-                        {'total_time':{}}
-                        ]
+                        {
+                            'total_score':data['total_score'],
+                            'total_time':{'$lt':data['total_time']}
+                        }]
                     }
-
             },
-
-    return rank, score, submit_time
+            {
+                '$group':{
+                        '_id':None,
+                        'count':{'$sum':1}
+                        }
+            }]
+    result = list(db.contests.aggregate(pipe_line2))
+    if len(result) == 0:
+        rank = 1
+    else:
+        rank = result[0]['count'] + 1
+    return rank, data['total_score'], data['total_time']
 
 if __name__ == '__main__':
     if(len(sys.argv) != 2):
         print("Parameter Error")
         exit()
     usr_id = int(sys.argv[1])
-    pint(get_rank(usr_id))
+    print(get_rank(usr_id))
