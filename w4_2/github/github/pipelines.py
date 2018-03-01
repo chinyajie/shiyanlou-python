e54f27336183470d02975348936317bf1d0e4bf3
chinyajie <qinyajie@msn.com>
2018-03-01 15:15:31 +0800

    finish w4_2

--- w4_2/github/github/pipelines.py
+++ w4_2/github/github/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from github.models import engine, Repository
 class GithubPipeline(object):
     def process_item(self, item, spider):
+        item['update_time'] = datetime.striptime(item['update_time'],
+                '%Y-%m-%dT%H:%M:%SZ')
+        self.session.add(Repository(**item))
         return item
+
+    def open_spider(self, spider):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def close_spide(self, spider):
+        self.session.commit()
+        self.session.close()
--- w4_2/github/github/spiders/github_r.py
+++ w4_2/github/github/spiders/github_r.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from github.items import GithubItem
 
 
 class GithubRSpider(scrapy.Spider):
     name = 'github_r'
     allowed_domains = ['github.com']
-    start_urls = ['http://github.com/']
+
+    @property
+    def start_urls(slef):
+        url = 'http://github.com/{}'
+        return (url.format(i) for i in range(1, 5)) 
 
     def parse(self, response):
-        pass
+        for repos in response.css('li[class="col-12 d-block width-full py-4 border-bottom public source"]'):
+            item = GithubItem({
+                'name': repos.css('div[class="d-inline-block mb-1"] h3 a::text').extract_first().strip('\n').strip(),
+                'update_time': repos.css('relative-time::attr(datetime)').extract_first()
+            })
+            yield item
