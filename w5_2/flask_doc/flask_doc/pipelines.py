814f4140588700bb6df927c0a125ddaf0a52e835
Arthur Qin <qinyajie@msn.com>
2018-03-06 17:17:30 +0800

    w5_2

--- w5_2/flask_doc/flask_doc/pipelines.py
+++ w5_2/flask_doc/flask_doc/pipelines.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+import redis
+import json
 
 
 class FlaskDocPipeline(object):
     def process_item(self, item, spider):
+        """
+        TODO: 将 item 结果以 JSON 形式保存到 Redis 数据库的 list 结构中
+        """
+        redis.lpush("flask_doc:items", json.dumps(item))
         return item
+
+def open_spider(self, spider):
+    # 连接数据库
+    self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def close_spider(self, spider):
+    # 关闭数据库连接
+    self.redis.close()
+
--- w5_2/flask_doc/flask_doc/spiders/flask.py
+++ w5_2/flask_doc/flask_doc/spiders/flask.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+from flask_doc.items import PageItem
+import re
 
-
-class FlaskSpider(scrapy.Spider):
+class FlaskSpider(scrapy.spiders.CrawlSpider):
     name = 'flask'
     allowed_domains = ['flask.pocoo.org']
-    start_urls = ['http://flask.pocoo.org/']
+    start_urls = "http://flask.pocoo.org/docs/0.12/"
 
-    def parse(self, response):
-        pass
+    linke = LinkExtractor(allow=(), deny=(), allow_domains=("http://flask.pocoo.org/docs/0.12/\.+", ),
+                                  deny_domains=(),deny_extensions=None, restrict_xpaths=(), restrict_css=(),
+                                  tags=('a', ), attrs=('href', ), canonicalize=True, unique=True, process_value=None)
+
+    rules = (
+        Rule(link_extractor=linke, callback="parse_page", follow=True),
+    )
+
+    def parse_page(self, response):
+        raw = response.xpath('/html/body/div[1]/div[2]/div[1]/div/div/text()').extract_first()
+        re.sub(re.compile(r'<[^>]+>'), "", raw)
+        re.sub(re.compile('\s+'), "\s", raw)
+
+        item = PageItem({
+            'url': str(response.url),
+            'text': raw,
+        })
+        yield item
