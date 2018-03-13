87df41fd174d1e14f32136c592c8710ee473451a
chinyajie <qinyajie@msn.com>
2018-03-13 13:42:35 +0800

    PASS:w6_1

---
+++ w6_1/analysis.py
@@ -0,0 +1,25 @@
+import json
+import pandas as pd
+import sys
+
+def analysis(file, user_id):
+    time = 0
+    minutes = 0
+    data = pd.read_json(file)
+    times = len(data[data['user_id']==user_id])
+    minutes =data[data['user_id']==user_id]['minutes'].sum()
+    return times, minutes
+
+
+if __name__ == '__main__':
+    if len(sys.argv)!=3:
+        print("Parameter Error, not enough!")
+        exit()
+    file = sys.argv[1]
+    try:
+        user_id = int(sys.argv[2])
+    except ValueError:
+        print("Parameter Error")
+        exit()
+    print(analysis(file, user_id))
+    
