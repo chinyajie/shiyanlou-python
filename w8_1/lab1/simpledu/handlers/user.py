2c2a22a63270180f673e8b2ee65c516d5a212fc9
Arthur Qin <qinyajie@msn.com>
2018-05-08 16:52:30 +0800

    fix an error

--- w8_1/lab1/simpledu/handlers/user.py
+++ w8_1/lab1/simpledu/handlers/user.py
@@ -3,12 +3,6 @@ from simpledu.models import db, Course, User
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
-@user.route('/<int>')
-def admin_index():
-    return 'admin'
-
-
-
 @user.route('<string:user_name>')
 def user(user_name):
     return render_template("user.html", user=User.query.get_or_404(username=user_name))
