3e995b366c133212073f77a4aa4e26fac93f4073
Arthur Qin <qinyajie@msn.com>
2018-06-13 16:51:09 +0800

    fix a typp in w9_1

--- w9_1/simpledu/handlers/live.py
+++ w9_1/simpledu/handlers/live.py
@@ -7,6 +7,6 @@ live = Blueprint('live', __name__, url_prefix='/live')
 
 @live.route('/')
 def index():
-    live = Live.query.order_by(Live.create_at.desc()).limit(1).first()
+    live = Live.query.order_by(Live.created_at.desc()).limit(1).first()
     return render_template('live/index.html', live=live)
 
