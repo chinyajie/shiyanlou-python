8db97a91e58a48d7841c3b0e4cc8d18eaa721493
chinyajie <qinyajie@msn.com>
2018-06-17 15:24:38 +0800

    make it feet the requirement

--- w9_3/simpledu/handlers/live.py
+++ w9_3/simpledu/handlers/live.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template ,redirect, url_for
- 
+from flask import Blueprint, render_template ,redirect, url_for,
+redirect, url_for, flash
+from simpledu.decorators import admin_required
+from simpledu.models import Course
+from simpledu.forms import CourseForm, MessageForm
+import json
+from .ws import redis 
 
 live = Blueprint('live', __name__, url_prefix='/live')
 
@@ -10,6 +15,12 @@ def index():
 
 @live.route('/systemmessage', methods=['GET', 'POST'])
 def message():
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    return redirect(url_for('admin.send_message'))
-
+    form = MessageForm()
+    if form.validate_on_submit():
+        redis.publish('chat', json.dumps(dict(
+            username='System',
+            text=form.text.data
+        )))
+        flash('系统消息发送成功', 'success')
+        return redirect(url_for('admin.index'))
+    return render_template('admin/message.html', form=form)
